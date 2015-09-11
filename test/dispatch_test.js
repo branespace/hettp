840b1decc61191a9eaafa64907cf75a16491f717
branespace <branespace@users.noreply.github.com>
2015-09-10 19:57:08 -0700

    Document and update tests for dispatch, fix scoping issues

--- test/dispatch_test.js
+++ test/dispatch_test.js
@@ -1,25 +1,40 @@
 "use strict";
 
-var chai = require('chai'),
-    chaihttp = require('chai-http'),
-    expect = chai.expect,
-    Dispatch = require('./../lib/dispatch'),
-    dispatcher = new Dispatch();
+var chai = require('chai').expect,          //assertions library
+    Dispatch = require('./../lib/dispatch'),//Dispatch constructor
+    dispatcher = new Dispatch(),            //construct new dispatch object
+    config = {
+        alpha: 2                            //sample callback counter
+    };
 
-describe('dispatch', function(){
+describe('dispatcher', function(){
     before(function(){
+        //Add functions to queue
         dispatcher.use(testFunc);
-        dispatcher.use(callBackFunc);
+        dispatcher.use(testFunc);
+        dispatcher.use(testFunc);
+        dispatcher.use(doneFunc);
     });
     it('should dispatch functions', function(done){
-        dispatcher.exec(done);
+        //Begin dispatch
+        //If it doesn't time out, callbacks were successful
+        dispatcher.exec(null, done, config);
+    });
+    it('properly scope variables', function(){
+        //testFunc increments counter 3 times (2 + 3 = 5)
+        expect(config.alpha).to.be.equal(5);
     });
 });
 
-function testFunc(done, callback){
-    callback();
+//Test callbacks with variables in another scope
+function testFunc(req, res, callback){
+    /*jshint validthis:true*/
+    this.config.alpha++;
+    /*jshint validthis:false*/
+    callback(req, res);
 }
 
-function callBackFunc(done, callback){
-    done();
+//Terminate test
+function doneFunc(req, res, callback){
+    res();
 }
